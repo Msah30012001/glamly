91337073dc804db9552e7ffaa532d09ce57fc6b7
Manish Shambhoo Sah <msah74915@gmail.com>
2023-02-20 19:32:19 +0530

    bug: fixed

--- src/App/ProductList.jsx
+++ src/App/ProductList.jsx
@@ -20,128 +20,6 @@ const ProductList = () => {
     }
     console.log(param.slug);
   }, [dispatch, searchParams.get("s"), param.slug]);
-  // const data = [
-  //   {
-  //     title: "Fila Locker Room varsity Jacket",
-  //     thumbnail: "assets/img/shop/13.jpg",
-  //     thumbnailHover: "assets/img/shop/13-h1.jpg",
-  //     rate: "4",
-  //     price: "1000",
-  //     discount: "50",
-  //     desc: "Create a cool and sporty look with the FILA® Locker Room Varsity Jacket Comfortable cotton-blend fabrication Classic varsity jacket features brand details throughout Flat knit collar",
-  //     stock: "300",
-  //   },
-  //   {
-  //     title: "Juicy Couture Tricot Logo Stripe Jacket",
-  //     thumbnail: "assets/img/shop/12.jpg",
-  //     thumbnailHover: "assets/img/shop/12-h1.jpg",
-  //     rate: "5",
-  //     price: "1000",
-  //     discount: "50",
-  //     desc: "Create a cool and sporty look with the FILA® Locker Room Varsity Jacket Comfortable cotton-blend fabrication Classic varsity jacket features brand details throughout Flat knit collar",
-  //     stock: "300",
-  //   },
-  //   {
-  //     title: "New Balance Fresh Foam LAZR v1 Sport",
-  //     thumbnail: "assets/img/shop/07.jpg",
-  //     thumbnailHover: "assets/img/shop/07-h1.jpg",
-  //     rate: "5",
-  //     price: "1000",
-  //     discount: "50",
-  //     desc: "Create a cool and sporty look with the FILA® Locker Room Varsity Jacket Comfortable cotton-blend fabrication Classic varsity jacket features brand details throughout Flat knit collar",
-  //     stock: "300",
-  //   },
-  //   {
-  //     title: "New Balance Fresh Foam Kaymin",
-  //     thumbnail: "assets/img/shop/06.jpg",
-  //     thumbnailHover: "assets/img/shop/06-h1.jpg",
-  //     rate: "5",
-  //     price: "1000",
-  //     discount: "50",
-  //     desc: "Create a cool and sporty look with the FILA® Locker Room Varsity Jacket Comfortable cotton-blend fabrication Classic varsity jacket features brand details throughout Flat knit collar",
-  //     stock: "300",
-  //   },
-  //   {
-  //     title: "Water and Wind Resistant Insulated Ja...",
-  //     thumbnail: "assets/img/shop/05.jpg",
-  //     thumbnailHover: "assets/img/shop/05-h1.jpg",
-  //     rate: "4",
-  //     price: "1000",
-  //     discount: "50",
-  //     desc: "Create a cool and sporty look with the FILA® Locker Room Varsity Jacket Comfortable cotton-blend fabrication Classic varsity jacket features brand details throughout Flat knit collar",
-  //     stock: "300",
-  //   },
-  //   {
-  //     title: "Trans-Weight Hooded Wind and Water...",
-  //     thumbnail: "assets/img/shop/04.jpg",
-  //     thumbnailHover: "assets/img/shop/04-h1.jpg",
-  //     rate: "5",
-  //     price: "1000",
-  //     discount: "50",
-  //     desc: "Create a cool and sporty look with the FILA® Locker Room Varsity Jacket Comfortable cotton-blend fabrication Classic varsity jacket features brand details throughout Flat knit collar",
-  //     stock: "300",
-  //   },
-  //   {
-  //     title: "Madden by Steve Madden Cale 6",
-  //     thumbnail: "assets/img/shop/03.jpg",
-  //     thumbnailHover: "assets/img/shop/03-h1.jpg",
-  //     rate: "5",
-  //     price: "1000",
-  //     discount: "50",
-  //     desc: "Create a cool and sporty look with the FILA® Locker Room Varsity Jacket Comfortable cotton-blend fabrication Classic varsity jacket features brand details throughout Flat knit collar",
-  //     stock: "300",
-  //   },
-  //   {
-  //     title: "Calvin Klein Jeans Reflactive Logo Full zip",
-  //     thumbnail: "assets/img/shop/11.jpg",
-  //     thumbnailHover: "assets/img/shop/11-h1.jpg",
-  //     rate: "4",
-  //     price: "1000",
-  //     discount: "50",
-  //     desc: "Create a cool and sporty look with the FILA® Locker Room Varsity Jacket Comfortable cotton-blend fabrication Classic varsity jacket features brand details throughout Flat knit collar",
-  //     stock: "300",
-  //   },
-  //   {
-  //     title: "Fila Locker Room varsity Jacket",
-  //     thumbnail: "assets/img/shop/10.jpg",
-  //     thumbnailHover: "assets/img/shop/10-h1.jpg",
-  //     rate: "4",
-  //     price: "1000",
-  //     discount: "50",
-  //     desc: "Create a cool and sporty look with the FILA® Locker Room Varsity Jacket Comfortable cotton-blend fabrication Classic varsity jacket features brand details throughout Flat knit collar",
-  //     stock: "300",
-  //   },
-  //   {
-  //     title: "Fila Locker Room varsity Jacket",
-  //     thumbnail: "assets/img/shop/09.jpg",
-  //     thumbnailHover: "assets/img/shop/02-h1.jpg",
-  //     rate: "5",
-  //     price: "1000",
-  //     discount: "50",
-  //     desc: "Create a cool and sporty look with the FILA® Locker Room Varsity Jacket Comfortable cotton-blend fabrication Classic varsity jacket features brand details throughout Flat knit collar",
-  //     stock: "300",
-  //   },
-  //   {
-  //     title: "Fila Locker Room varsity Jacket",
-  //     thumbnail: "assets/img/shop/02.jpg",
-  //     thumbnailHover: "assets/img/shop/02-h1.jpg",
-  //     rate: "4",
-  //     price: "1000",
-  //     discount: "50",
-  //     desc: "Create a cool and sporty look with the FILA® Locker Room Varsity Jacket Comfortable cotton-blend fabrication Classic varsity jacket features brand details throughout Flat knit collar",
-  //     stock: "300",
-  //   },
-  //   {
-  //     title: "Fila Locker Room varsity Jacket",
-  //     thumbnail: "assets/img/shop/08.jpg",
-  //     thumbnailHover: "assets/img/shop/08-h1.jpg",
-  //     rate: "4",
-  //     price: "1000",
-  //     discount: "50",
-  //     desc: "Create a cool and sporty look with the FILA® Locker Room Varsity Jacket Comfortable cotton-blend fabrication Classic varsity jacket features brand details throughout Flat knit collar",
-  //     stock: "300",
-  //   },
-  // ];
   return (
     <>
       <Breadcrumb />
@@ -164,6 +42,15 @@ const ProductList = () => {
                 </div>
               );
             })}
+            {productList.data.length === 0 && (
+              <>
+                <i
+                  className="fa fa-circle-exclamation fa-bounce text-danger text-center"
+                  style={{ fontSize: "15em" }}
+                ></i>
+                <h2 className="text-danger text-center">Oops ! Cosmetic not available</h2>
+              </>
+            )}
           </div>
         </div>
       </section>
